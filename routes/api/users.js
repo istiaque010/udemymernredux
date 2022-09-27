9afea6e2a5cf92565f25bbdbc41594650ce03a59
isti4github <isti.ru.cse@gmail.com>
2022-09-27 18:25:29 +0900

    start working on models user model created and registered one user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,14 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require("express-validator");
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+
+
+
+const User = require('../../models/Users');
 
 //@route POST api/users
 //@desv Register user
@@ -13,7 +21,7 @@ router.post(
     check('password','Please enter a password with 6 or more characters').isLength({min:6})
 
 ], 
-  (req, res) => {
+  async (req, res) => {
   //console.log(req.body);
 
   const errors = validationResult(req);
@@ -23,7 +31,58 @@ router.post(
     return res.status(400).json({errors:errors.array()});
   }
 
-  res.send("User route");
+  const {name,email,password}=req.body;
+
+  try{
+
+  //see if user exists
+   let user= await User.findOne({email});
+
+   if(user){
+    return res.status(400).json({errors: [{msg: 'User already exists'}]});
+   }
+
+  //get user gravetor
+
+  const avatar = gravatar.url(email,{
+    s:'200',
+    r:'pg',
+    d:'mm'
+    })
+
+    user = new User({
+      name,
+      email,
+      avatar,
+      password
+    });
+
+  //encript password
+
+  const salt = await bcrypt.genSalt(10);
+
+  user.password = await bcrypt.hash(password,salt);
+
+  await user.save();
+
+  //return jsonwebtoken
+
+    res.send("User registered");
+
+  }catch(err){
+
+    console.error(err.message);
+
+  }
+
+
+
+
+
+
+
+
+
 });
 
 module.exports = router;
